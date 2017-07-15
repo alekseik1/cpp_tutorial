47584084cb95110e71b7c39494aa74470c8472ae
alekseik1 <alekseik1@yandex.ru>
2017-07-15 14:38:32 +0300

    Сделал очередь. Внимание! Код заряжен костылями

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -5,20 +5,54 @@
 
 using namespace std;
 
-bool IsPalindrom(string s) {
-	string s1 = s;
-	std::reverse(s.begin(), s.end());
-	if (s1 == s)
-		return true;
-	return false;
+void PrintVector(const vector<int>& v) {
+	int i = 0;
+	for (auto s : v) {
+		cout << i << ": " << s << endl;
+		++i;
+	}
 }
 
-vector<string> PalindromFilter(vector<string> words, int minLength) {
-	vector<string> res;
-	for (auto c : words) {
-		if (IsPalindrom(c) && c.size() >= minLength) {
-			res.push_back(c);
+int main() {
+	int Q;
+	int curr_man = 0;
+	cin >> Q;
+	vector<int> worry;
+	vector<int> queue;
+	for (int i = 0; i < Q; ++i) {
+		string op;
+		int k;
+		cin >> op;
+		if (op != "WORRY_COUNT") {
+			cin >> k;
+			if (op == "COME") {
+				if (k > 0) {
+					for (int j = 0; j < k; j++) {
+						queue.push_back(curr_man);
+						++curr_man;
+					}
+				} else if (k < 0) {
+					for (int j = 0; j < -k; j++) {
+						for(int gav = 0; gav < worry.size(); gav++) {
+							if (queue.back() == worry[gav]) {
+								worry.erase(worry.begin() + gav);
+							}
+						}
+						queue.pop_back();
+						--curr_man;
+					}
+				}
+			} else if (op == "WORRY") {
+				worry.push_back(queue[k]);
+			} else if (op == "QUIET") {
+				auto pos = std::find(worry.begin(), worry.end(), k);
+				worry.erase(pos);
+			}
+		} else {
+			cout << worry.size() << endl;
+			continue;
 		}
 	}
-	return res;
+	return 0;
 }
+
