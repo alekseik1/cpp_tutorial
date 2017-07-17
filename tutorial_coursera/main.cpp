14204f0cf02649f10454ca914adad008cefb9560
alekseik1 <alekseik1@yandex.ru>
2017-07-17 09:39:12 +0300

    Сделал месячные дни.

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -5,52 +5,74 @@
 
 using namespace std;
 
-void PrintVector(const vector<int>& v) {
-	int i = 0;
-	for (auto s : v) {
-		cout << i << ": " << s << endl;
-		++i;
+struct delo {
+	string name;
+	int month;
+	int day;
+};
+
+void AddDelo(vector<delo>& dela, string name, int month, int day) {
+	dela.push_back({name, month, day});
+}
+
+void FinishMonth(vector<delo>& dela, int& curr_month) {
+	if (curr_month == 12) {	// Январь
+		curr_month = 1;
+		return;
 	}
+	vector<int> days_in_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	if (days_in_month[curr_month] >= days_in_month[curr_month - 1]) {
+		curr_month++;
+		return;
+	} else {
+		for (auto& i : dela) {
+			if (i.day > days_in_month[curr_month]) {
+				i.day = days_in_month[curr_month];	// Все откладываем на последний день
+			}
+		}
+		curr_month++;
+	}
+}
+
+void DumpDay(vector<delo>& dela, int day) {
+	int count = 0;
+	for (delo i : dela) {
+		if(i.day == day) {
+			count++;
+		}
+	}
+	if(count == 0) {
+		cout << 0 << endl;
+		return;
+	}
+	cout << count << " ";
+	for (delo i : dela) {
+		if(i.day == day) {
+			cout << i.name << " ";
+		}
+	}
+	cout << endl;
 }
 
 int main() {
 	int Q;
-	int curr_man = 0;
+	int curr_month = 1;
 	cin >> Q;
-	vector<int> worry;
-	vector<int> queue;
-	for (int i = 0; i < Q; ++i) {
-		string op;
-		int k;
-		cin >> op;
-		if (op != "WORRY_COUNT") {
-			cin >> k;
-			if (op == "COME") {
-				if (k > 0) {
-					for (int j = 0; j < k; j++) {
-						queue.push_back(curr_man);
-						++curr_man;
-					}
-				} else if (k < 0) {
-					for (int j = 0; j < -k; j++) {
-						for(int gav = 0; gav < worry.size(); gav++) {
-							if (queue.back() == worry[gav]) {
-								worry.erase(worry.begin() + gav);
-							}
-						}
-						queue.pop_back();
-						--curr_man;
-					}
-				}
-			} else if (op == "WORRY") {
-				worry.push_back(queue[k]);
-			} else if (op == "QUIET") {
-				auto pos = std::find(worry.begin(), worry.end(), k);
-				worry.erase(pos);
-			}
-		} else {
-			cout << worry.size() << endl;
-			continue;
+	vector<delo> dela;
+	string command;
+	for (int i = 0; i < Q; i++) {
+		cin >> command;
+		if(command == "NEXT") {
+			FinishMonth(dela, curr_month);
+		} else if(command == "DUMP") {
+			int day;
+			cin >> day;
+			DumpDay(dela, day);
+		} else if(command == "ADD") {
+			int day;
+			string name;
+			cin >> day >> name;
+			AddDelo(dela, name, curr_month, day);
 		}
 	}
 	return 0;
