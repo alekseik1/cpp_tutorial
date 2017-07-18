887cea572418e9b12c8e8b1f86a88fbcd965afa6
alekseik1 <alekseik1@yandex.ru>
2017-07-18 21:08:54 +0300

    Сделал автобусные остановки. Костыли included :)

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -17,6 +17,7 @@ int main() {
 	int Q;
 	string command;
 	map<string, vector<string>> stops_for_bus;
+	vector<string> crutch;	
 	cin >> Q;
 	for(int i = 0; i < Q; i++) {
 		cin >> command;
@@ -25,6 +26,7 @@ int main() {
 			string stop;
 			string bus;
 			cin >> bus >> n;
+			crutch.push_back(bus);	// Топовый костыль: в отдельную очередь запихнем порядок ввода автобусов. И зачем авторы задания просят это делать?
 			vector<string> route;
 			for(int j = 0; j < n; j++) {
 				cin >> stop;
@@ -35,11 +37,18 @@ int main() {
 		} else if(command == "BUSES_FOR_STOP") {
 			string stop; cin >> stop;
 			vector<string> tmp_buses;
-			for(auto bus_and_route : stops_for_bus) {
+			
+            		for(auto bus_i : crutch) {
+                    		if(has(stops_for_bus[bus_i], stop)) {
+                        		tmp_buses.push_back(bus_i);
+                    		}
+            		}
+
+			/*for(auto bus_and_route : stops_for_bus) {
 				if(has(bus_and_route.second, stop)) {	// Если автобус проезжает через остановку
 					tmp_buses.push_back(bus_and_route.first);
 				}
-			}
+			}*/
 			if(tmp_buses.size() == 0) {
 				cout << "No stop" << endl;
 				continue;
@@ -50,7 +59,7 @@ int main() {
 				cout << endl;
 			}
 		} else if(command == "STOPS_FOR_BUS") {	// Написал после стакана вина. Не спрашивайте, как. Тееперь и работает
-			string bus; cin >> bus;
+			string bus; cin >> bus;		// TODO: тут надо выводить в порядке, в котором они были занесены. Ужас!
 			bool fl = true;
 			if(stops_for_bus.count(bus) == 0) {
 				cout << "No bus";
@@ -59,9 +68,9 @@ int main() {
 			}
 			for(auto stop : stops_for_bus[bus]) {
 				cout << "Stop " << stop << ": ";
-				for(auto route_and_bus : stops_for_bus) {
-					if(has(route_and_bus.second, stop) && route_and_bus.first != bus) {
-						cout << route_and_bus.first << " ";
+				for(auto bus_i : crutch) {
+					if(has(stops_for_bus[bus_i], stop) && bus_i != bus) {
+						cout << bus_i << " ";
 						fl = false;
 					}
 				}
