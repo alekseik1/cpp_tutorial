b9ebf65c4d4fe8416a5e607c5e0c3f7935d05545
alekseik1 <alekseik1@yandex.ru>
2017-07-19 23:07:33 +0300

    Что-то сделал из синонимов, но не проходит все тесты

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -3,40 +3,68 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <set>
 
 using namespace std;
 
-int has(const vector<vector<string>>& v, const vector<string>& el) {
-	for(unsigned i = 0; i < v.size(); i++) {
-		if(v[i] == el) {
-			return i;
+struct Pair {
+	string word;
+	vector<string> nrs;
+
+	bool hasNeighbour(string& nr) {
+		for(auto& k : nrs) {
+			if(nr == k) {
+				return true;
+			}
 		}
+		return false;
 	}
-	return -1;
-}
+};
 
 int main() {
-	int Q;
-	cin >> Q;
-	vector<vector<string>> routes;
-	vector<string> curr;
-	string tmp;
-	for(int j = 0; j < Q; j++) {
-		int n; cin >> n;
-		for(int i = 0; i < n; i++ ) {
-			cin >> tmp;
-			curr.push_back(tmp);
+	int Q; cin >> Q;
+	vector<Pair> syn_pairs;
+	for(int asd = 0; asd < Q; asd++) {
+		string command; cin >> command;
+		if(command == "ADD") {
+			string w1, w2; cin >> w1 >> w2;
+			bool fl = true;
+			for(Pair& p : syn_pairs) {
+				if(p.word == w1) {	// Уже добавляем к слову в словаре
+					p.nrs.push_back(w2);
+					fl = false;
+					break;
+				}
+			}
+			if(fl) {	// Впервые встречаем слово, значит, для него делаем запись
+				Pair tmp = {w1, {w2}};
+				syn_pairs.push_back(tmp);
+			}
+			fl = true;
+		} else if(command == "COUNT") {
+			string w; cin >> w;
+			int k = 0;
+			for(Pair p : syn_pairs) {
+				if(p.word == w || p.hasNeighbour(w)) {
+					k++;
+				}
+			}
+			cout << k << endl;
+		} else if(command == "CHECK") {
+			string w1, w2; cin >> w1 >> w2;
+			bool fl = false;
+			for(Pair p : syn_pairs) {
+				if((p.word == w1 && p.hasNeighbour(w2)) || (p.word == w2 && p.hasNeighbour(w1))) {
+					fl = true;
+					break;
+				}
+			}
+			if(fl) {
+				cout << "YES" << endl;
+			} else {
+				cout << "NO" << endl;
+			}
 		}
-		int t = has(routes, curr);
-		if(t == -1) {
-			routes.push_back(curr);
-			cout << "New bus " << routes.size();
-			curr.clear();
-		} else {
-			cout << "Already exists for " << t+1;
-		}
-		cout << endl;
 	}
 	return 0;
 }
-
