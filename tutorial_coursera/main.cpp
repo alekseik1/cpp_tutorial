a9aae5c4ecd3ecea4baf26dee7d69b448dead818
alekseik1 <alekseik1@yandex.ru>
2017-07-20 18:57:20 +0300

    Сортировка без учета регистра

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -4,9 +4,28 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <locale>
 
 using namespace std;
 
 int main() {
+	int N; cin >> N;
+	vector<string> v;
+	for(int i = 0; i < N; i++) {
+		string tmp; cin >> tmp;
+		v.push_back(tmp);
+	}
+	sort(begin(v), end(v), [](string a, string b) {
+		for(char& c : a) {
+			c = tolower(c);
+		}
+		for(char& c : b) {
+			c = tolower(c);
+		}
+		return a < b;
+	});
+	for(auto& k : v) {
+		cout << k << " ";
+	}
 	return 0;
 }
