b9a96749e67a0afa9d319396fd62cde2e952bc87
alekseik1 <alekseik1@yandex.ru>
2017-07-20 18:36:56 +0300

    Сортировка по модулю

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -4,29 +4,22 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-	int Q; cin >> Q;
-	map<string, set<string>> syn_pairs;
-	for(int asd = 0; asd < Q; asd++) {
-		string command; cin >> command;
-		if(command == "ADD") {
-			string w1, w2; cin >> w1 >> w2;
-			syn_pairs[w1].insert(w2);
-			syn_pairs[w2].insert(w1);
-		} else if(command == "COUNT") {
-			string w; cin >> w;
-			cout << syn_pairs[w].size() << endl;
-		} else if(command == "CHECK") {
-			string w1, w2; cin >> w1 >> w2;
-			if(syn_pairs[w1].count(w2) > 0) {
-				cout << "YES" << endl;
-			} else {
-				cout << "NO" << endl;
-			}
-		}
+	int N; cin >> N;
+	vector<int> num;
+	for(int i = 0; i < N; i++) {
+		int k; cin >> k;
+		num.push_back(k);
+	}
+	sort(begin(num), end(num), [](int a, int b) {
+		return(abs(a) < abs(b));
+	});
+	for(auto& e : num) {
+		cout << e << " ";
 	}
 	return 0;
 }
