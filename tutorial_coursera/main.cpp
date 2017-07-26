8b8221fdb2d6b869bb00b250ce0285b9480f8be9
alekseik1 <alekseik1@yandex.ru>
2017-07-26 16:47:05 +0300

    Сделал что-то из Имя_Фамилия, но не проходит последний тест

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -7,6 +7,46 @@
 
 using namespace std;
 
-int main() {
-	return 0;
-}
+class Person {
+public:
+  void ChangeFirstName(const int& year, const string& first_name) {
+    // добавить факт изменения имени на first_name в год year
+	  n_h[year] = first_name;
+  }
+  void ChangeLastName(const int& year, const string& last_name) {
+    // добавить факт изменения фамилии на last_name в год year
+	  s_h[year] = last_name;
+  }
+  string GetFullName(int year) {
+    // получить имя и фамилию по состоянию на конец года year
+	  string surname = "";
+	  string name = "";
+	  //sort(s_h.begin(), s_h.end());
+	  for(auto& i : s_h) {
+		  if(i.first <= year) {
+			  surname = i.second;
+		  }
+	  }
+	  //sort(n_h.begin(), n_h.end());
+	  for(auto& i : n_h) {
+		  if(i.first <= year) {
+			  //cout << i.first << endl;
+			  name = i.second;
+		  }
+	  }
+	  if(name.size() == 0 && surname.size() == 0) {
+		  return "Incognito";
+	  } else if(name.size() == 0) {
+		  return surname + " with unknown name";
+	  } else if(surname.size() == 0) {
+		  return name + " with unknown last name";
+	  } else {
+		  return name + " " + surname;
+	  }
+  }
+private:
+  // приватные поля
+  map<int, string> s_h;
+  map<int, string> n_h;
+};
+
