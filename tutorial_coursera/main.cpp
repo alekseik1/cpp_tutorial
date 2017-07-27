cfca466e9fb6b23b1fce5f9d5468e0db69e8c7e7
alekseik1 <alekseik1@yandex.ru>
2017-07-27 16:44:01 +0300

    Имя и фамилия 2. Очень сильное колдунство :)

--- tutorial_coursera/main.cpp
+++ tutorial_coursera/main.cpp
@@ -44,9 +44,102 @@ public:
 		  return name + " " + surname;
 	  }
   }
+
+  string GetFullNameWithHistory(const int& year) {
+	  string curr_name = "";
+	  vector<string> name_stack;
+	  for(auto& i : n_h) {
+		  if(i.first <= year && i.second != curr_name) {
+			  curr_name = i.second;
+			  name_stack.push_back(i.second);
+		  }
+	  }	// Сделали текущее имя, ура!
+	  string curr_surname = "";
+	  vector<string> surname_stack;
+	  for(auto& i : s_h) {
+		  if(i.first <= year && i.second != curr_surname) {
+			  curr_surname = i.second;
+			  surname_stack.push_back(i.second);
+		  }
+	  }
+	  string name_part = "";
+	  if(curr_name.size() != 0) {
+		  name_part = curr_name;
+		  if(name_stack.size() > 0) {
+			  checkTheFuckingListAndReverseIt(name_stack, curr_name);
+			  //reverse(name_stack.begin(), name_stack.end());
+		  }
+		  if(name_stack.size() > 0) {
+			  name_part += " (";
+			  for(auto& s : name_stack) {
+					  name_part += s;
+				  if(s != name_stack.back()) {
+					  name_part += ", ";
+				  } else {
+					  name_part += ") ";
+				  }
+			  }
+		  } else {
+			  name_part += " ";
+		  }
+	  }
+	  string surname_part = "";
+	  if(curr_surname.size() != 0) {
+		  surname_part = curr_surname;
+		  if(surname_stack.size() > 0) {
+			  checkTheFuckingListAndReverseIt(surname_stack, curr_surname);
+			  //reverse(surname_stack.begin(), surname_stack.end());
+		  }
+		  if(surname_stack.size() > 0) {
+			  surname_part += " (";
+			  for(unsigned j = 0; j < surname_stack.size(); j++) {
+				  string s = surname_stack[j];
+				  surname_part += s;
+				  if(j != surname_stack.size() - 1) {
+					  surname_part += ", ";
+				  } else {
+					  surname_part += ") ";
+				  }
+			  }
+		  } else {
+			  surname_part += " ";
+		  }
+	  }
+	  if(name_part.size() == 0 && surname_part.size() == 0) {
+		  return "Incognito";
+	  } else if(name_part.size() == 0) {
+		  return surname_part + "with unknown first name";
+	  } else if(surname_part.size() == 0) {
+		  return name_part + "with unknown last name";
+	  } else {
+		  string tmp = name_part + surname_part;
+		  rtrim(tmp);
+		  return tmp;
+	  }
+	  return "";
+  }
 private:
+  static inline void rtrim(std::string &s) {
+      s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+          return !std::isspace(ch);
+      }).base(), s.end());
+  }
+  void checkTheFuckingListAndReverseIt(vector<string>& list, string& first_item) {
+	  reverse(list.begin(), list.end());
+	  if(first_item == list[0]) {
+		  list.erase(list.begin());
+	  }
+	  if(list.size() == 0) {
+		  return;
+	  }
+	  for(unsigned i = 0; i < list.size() - 1; i++) {
+		  if(list[i] == list[i+1]) {
+			  list.erase(list.begin() + i);
+			  i--;
+		  }
+	  }
+  }
   // приватные поля
   map<int, string> s_h;
   map<int, string> n_h;
 };
-
